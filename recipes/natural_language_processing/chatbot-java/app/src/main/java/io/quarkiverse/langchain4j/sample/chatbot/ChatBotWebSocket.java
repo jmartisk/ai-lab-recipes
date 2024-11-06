9e349fa19d429dfce459ca968ff8a884756e9c0b
Jan Martiska <jmartisk@redhat.com>
2024-11-06 13:05:59 +0100

    Recipe for a simple Quarkus-based chatbot
    Signed-off-by: Jan Martiska <jmartisk@redhat.com>

---
+++ recipes/natural_language_processing/chatbot-java/app/src/main/java/io/quarkiverse/langchain4j/sample/chatbot/ChatBotWebSocket.java
@@ -0,0 +1,22 @@
+package io.quarkiverse.langchain4j.sample.chatbot;
+
+import io.quarkus.websockets.next.OnOpen;
+import io.quarkus.websockets.next.OnTextMessage;
+import io.quarkus.websockets.next.WebSocket;
+import io.smallrye.mutiny.Multi;
+
+@WebSocket(path = "/chatbot")
+public class ChatBotWebSocket {
+
+    private final Bot bot;
+
+    public ChatBotWebSocket(Bot bot) {
+        this.bot = bot;
+    }
+
+    @OnTextMessage
+    public Multi<String> onMessage(String message) {
+        return bot.chat(message);
+    }
+
+}
